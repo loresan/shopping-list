52495b30c3ac65414b4b894ac9113cc513d31fd8
loresan <loresan84@gmail.com>
2015-12-01 22:30:10 +0700

    modified:   css/main.css 	modified:   images/checked.png 	modified:   images/unchecked.png 	modified:   index.html 	new file:   js/jquery-2.1.4.min.js 	modified:   js/main_script.js

---
+++ js/main_script.js
@@ -0,0 +1,96 @@
+$(document).ready(initializeForm);
+
+function initializeForm() {
+
+    // initialize the app
+    var itemCount = 0;
+    var itemInput = $("#item");
+    var price = $('#price');
+    var qnt = $('#qnt');
+    itemInput.val("");
+    price.val("");
+    qnt.val("");
+    // define what happens when we click the "Add Item" link
+    $("#add_item").click( function(){
+             shoppingList();
+           }
+        );
+    $("input#item").keydown(function (enter) {
+        if (enter.keyCode == 13) {
+            shoppingList();
+        }
+    });
+
+    function shoppingList() {
+        if (itemInput.val() === '' || $.isNumeric(itemInput.val()) ||  price.val() === '' || qnt.val() === ''){
+            return;
+        }
+        // get the items list and item to add
+        var items = $("ul.list_items");
+        var itemToBuy = itemInput.val();
+        var priceShow  = price.val();
+        var qntItem = qnt.val();
+		itemInput.val("");
+        price.val("");
+        qnt.val("");
+        var white_space= "&nbsp;&nbsp;&nbsp;&nbsp;";
+        var finalPrice = priceShow * qntItem;
+        
+        // create a list item and checkbox, assigning an id to it.
+        var listItem = $("<li> " + "<span class='item_text'>"+itemToBuy+"</span>" + white_space + "$ <span class='price_box'>  " + finalPrice +"</span> <span class='img_box'><input type='checkbox' class='cbox'><img class='bin' src='images/bin.png'><img class='edit' src='images/edit.png'></span></li>");
+        listItem.attr("id", "item[" + itemCount+++"]");
+		addItem(listItem);
+
+        //delete items
+        $(".bin").click(function () {
+            $(this).parent().parent().hide('slow', function () {
+                $(this).remove();
+            });
+        });
+        
+        //checked bought items and add Total
+        $('.cbox').change(function(){
+            var total = 0;
+            $('.cbox:checked').each(function(){
+                    total+=parseInt($(this).parent().prev().html());
+            });
+              
+               $("#total").text('$' + total);
+        });
+
+        //edit items and add Total
+        $('.edit').click(function() {
+            var text = $('.item_text').text();  
+            var input = $('<input type="text" class="hidden"  value="'+ text +'" />')
+            $(this).each(function(){
+                    $(this).parent().prev().prev().text('').append(input);
+            }); 
+            
+            $('input.hidden').blur(function() { 
+              var defaultValue =  $(this).val();
+              $(this).parent().text(defaultValue);
+              $('input.hidden').hide(); 
+                                  
+            });
+
+            $('input[type="text"]').keypress(function(event) {
+                if (event.keyCode == '13') {
+                      var defaultValue =  $(this).val();
+                      $(this).parent().prev().prev().text(defaultValue);
+                      $('input.hidden').hide();
+                   }
+            });
+        }); 
+
+
+        // add the item to the list. initially hidden, then slide in slowly
+        function addItem(listItem) {
+            listItem.hide();
+            items.append(listItem);
+            listItem.show('slow');
+        }
+        // clear input and refocus
+        itemInput.focus();
+    }
+}
+ 
